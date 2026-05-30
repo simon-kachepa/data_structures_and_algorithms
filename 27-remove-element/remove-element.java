0dc7010595eab3a19625ff8e59bb6e37a2b46d24
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2026-05-30 10:22:33 +0200

    Time: 0 ms (100.00%) | Memory: 43.7 MB (22.71%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -3,10 +3,10 @@ class Solution {
         List<Integer> myList = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++){
-            myList.add(nums[i]);
+            if (nums[i] != val){
+                myList.add(nums[i]);
+            }
         }
-
-        myList.removeIf(n -> n == val);
 
         for (int i = 0; i < myList.size(); i++){
             nums[i]= myList.get(i);
