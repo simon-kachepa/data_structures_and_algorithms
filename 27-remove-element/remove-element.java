9d0bbc7828cf07414927e1dbe74cdf7d10862cc1
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2026-06-08 08:12:22 +0200

    Time: 0 ms (100.00%) | Memory: 43.4 MB (59.93%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -4,13 +4,6 @@ class Solution {
         int j = nums.length - 1;
 
         for (int i = 0; i <= j; i++){
-            if (i == j && nums[i] != val){
-                count++;
-                break;
-            }
-            else if (i == j && nums[i] == val){
-                break;
-            }
             if (nums[i] != val){
                 count++;
                 continue;
