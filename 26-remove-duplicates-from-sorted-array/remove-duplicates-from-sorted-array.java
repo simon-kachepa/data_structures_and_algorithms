6ec3a9057a15442f3c74e685b5af271637acf5c1
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2026-08-05 23:55:55 +0200

    Added formating

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -2,9 +2,11 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 0;
         int count = 1;
+        
         if(nums.length == 0){
             return 0;
         }
+        
         for (int j = 1; j < nums.length; j++){
             if (nums[i] != nums[j]){
                 i++;
